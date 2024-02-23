03546ec1810f555aeec9babb64363d45d5534b27
katayouny <k.yousefzadeh@gmail.com>
2024-02-23 13:09:51 +0100

    Meal-sharing endpoints

--- src/backend/app.js
+++ src/backend/app.js
@@ -8,6 +8,9 @@ const buildPath = path.join(__dirname, "../../dist");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 
+//to get connected to the database
+const knex = require("./database");
+
 // For week4 no need to look into this!
 // Serve the built client html
 app.use(express.static(buildPath));
@@ -21,10 +24,121 @@ app.use(cors());
 
 router.use("/meals", mealsRouter);
 
+//------------------------------------------------
+/*app.get("/my-route", (req, res) => {
+  res.send("Here you will see the Routes.");
+});
+console.log("Good, the connection to database is working");*/
+//--------------------------------------------------
+
+// Routes
+
+//future-meals	Respond with all meals in the future (relative to the meal_time datetime)
+//respond with a collection of meals, meaning an array of objects.
+//running from http://localhost:5000/future-meals
+
+const todayDate = new Date();
+app.get("/future-meals", async (req, res) => {
+  try {
+    const futureMeals = await knex
+      .select("*")
+      .from("meal")
+      .where("meal_time", ">", todayDate);
+    if (futureMeals !== 0) {
+      res.status(200).json(futureMeals);
+    }
+    res
+      .status(404)
+      .send("The data you have requested for Future Meals is not found");
+  } catch (err) {
+    console.error("Server Error", err);
+    res.status(500).send("Error: Not able to fetch data for future meals");
+  }
+});
+
+//past-meals	Respond with all meals in the past (relative to the meal_time datetime)
+app.get("/past-meals", async (req, res) => {
+  try {
+    const pastMeals = await knex
+      .select("*")
+      .from("meal")
+      .where("meal_time", "<", todayDate);
+    if (pastMeals !== 0) {
+      res.status(200).json(pastMeals);
+    }
+    res
+      .status(404)
+      .send("The data you have requested for Past Meals is not found");
+  } catch (err) {
+    console.error("Server Error", err);
+    res.status(500).send("Error: Not able to fetch data for past meals");
+  }
+});
+
+//all-meals	Respond with all meals sorted by ID - respond with a collection of meals, meaning an array of objects.
+app.get("/all-meals", async (req, res) => {
+  try {
+    const allMeals = await knex.select("*").from("meal").orderBy("id"); // Sorting by ID
+    if (allMeals !== 0) {
+      res.status(200).json(allMeals);
+    }
+    res.status(404).send("The data you have requested for meals is not found");
+  } catch (err) {
+    console.error("Server Error", err);
+    res.status(500).send("Error: Not able to fetch data for meals");
+  }
+});
+
+//first-meal	Respond with the first meal (meaning with the minimum id) -respond with a single meal, meaning an object
+app.get("/first-meal", async (req, res) => {
+  try {
+    const firstMeal = await knex
+      .select("*")
+      .from("meal")
+      .orderBy("id")
+      .limit(1); // Limiting the result to just one meal
+    if (firstMeal !== 0) {
+      res.status(200).json(firstMeal);
+    }
+    res
+      .status(404)
+      .send(
+        "The data you have requested for First Meal with the minimum ID is not found"
+      );
+  } catch (err) {
+    console.error("Server Error", err);
+    res.status(500).send("Error: Not able to fetch data for meals");
+  }
+});
+
+//last-meal	Respond with the last meal (meaning with the maximum id)- respond with a single meal, meaning an object
+app.get("/last-meal", async (req, res) => {
+  try {
+    const lastMeal = await knex
+      .select("*")
+      .from("meal")
+      .orderBy("id", "desc")
+      .limit(1);
+    if (lastMeal !== 0) {
+      res.status(200).json(lastMeal);
+    }
+    res
+      .status(404)
+      .send(
+        "The data you have requested for First Meal with the minimum ID is not found"
+      );
+  } catch (err) {
+    console.error("Server Error", err);
+    res.status(500).send("Error: Not able to fetch data for meals");
+  }
+});
+
+//-----------------------------------------------------
+
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
 } else {
-  throw "API_PATH is not set. Remember to set it in your .env file"
+  throw "API_PATH is not set. Remember to set it in your .env file";
 }
 
 // for the frontend. Will first be covered in the react class
