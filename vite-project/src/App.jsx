04eb2738888808e827cce478c8d1378d8791645f
katayouny <k.yousefzadeh@gmail.com>
2024-04-18 11:29:39 +0200

    Meal-Sharing React2-Week1

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import MealsList from './components/mealsList';
+import './App.css'
+
+
+function App() {
+ 
+  return (
+    <>
+    <h2 className='welcome'>Welcome to Meal Sharing APP</h2>
+      <div>
+      <MealsList />
+      </div>
+    </>
+  )
+}
+
+
+export default App;
---
+++ vite-project/src/components/MealsList.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import Meal from "./Meal";
+import api_url from "../api-url";
+
+function MealsList() {
+  const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(api_url("/api/meals"))
+      .then((response) => response.json())
+      .then((data) => {
+        setMeals(data);
+        console.log(data);
+        setLoading(false);
+      })
+      .catch((error) => console.error("Error fetching meals:", error));
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <h4 className="list-title">List of meals and prices <p style={{fontSize:"10px"}}>Hover over images for enlargement</p></h4>
+          
+          <div className="grid-container">
+            <ul>
+            {meals.map((meal) => (
+              <Meal key={meal.id} meal={meal} />
+            ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default MealsList;
